4c33d4def9150bfc221a9f3ae768f9ac2a0f3435
kentatixx <kentatixx@gmail.com>
2023-10-28 01:27:46 +0800

    Add First Project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { projectspic } from "../assets";
+import { projectspic, project1 } from "../assets";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants.js";
 
@@ -26,20 +26,36 @@ const Projects = () => {
               variants={fadeIn("down", 0.7)}
               initial="hidden"
               whileInView={"show"}
-              className="group relative max-w-[200px] md:max-w-[300px] lg:max-w-full overflow-hidden border border-lightgray/50 rounded-lg"
+              className="group relative max-w-[200px] md:max-w-[500px]  overflow-hidden border border-lightgray/50 rounded-lg"
             >
               <div className=" cursor-pointer  group-hover:bg-black/70 w-full h-full absolute z-10 transition-all duration-300"></div>
 
               <img
                 className="  group-hover:scale-125 transition-all duration-500 "
-                src={projectspic}
+                src={project1}
               />
+
+              <div className="absolute -bottom-full left-12 group-hover:bottom-20 transition-all duration-700 z-50">
+                <span className="text-3xl text-white font-thin">
+                  {" "}
+                  Envogue Elite
+                </span>
+              </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-10 transition-all duration-500 z-50 ">
+                <span>
+                  <a href="https://envogueelite.vercel.app/" target="_blank">
+                    <button className="font- bg-bgcol text-black p-[2px] w-[90px] rounded">
+                      Live Demo
+                    </button>
+                  </a>
+                </span>
+              </div>
             </motion.div>
             <motion.div
               variants={fadeIn("down", 0.9)}
               initial="hidden"
               whileInView={"show"}
-              className="group relative max-w-[200px] md:max-w-[300px] lg:max-w-full overflow-hidden border border-black/50 rounded"
+              className="group relative max-w-[200px] md:max-w-[500px]  overflow-hidden border border-black/50 rounded"
             >
               <div className=" cursor-pointer  group-hover:bg-black/70 w-full h-full absolute z-10 transition-all duration-300"></div>
 
@@ -52,7 +68,7 @@ const Projects = () => {
               variants={fadeIn("down", 1)}
               initial="hidden"
               whileInView={"show"}
-              className="group relative max-w-[200px] md:max-w-[300px] lg:max-w-full overflow-hidden border border-black/50 rounded"
+              className="group relative max-w-[200px] md:max-w-[500px]  overflow-hidden border border-black/50 rounded"
             >
               <div className=" cursor-pointer  group-hover:bg-black/70 w-full h-full absolute z-10 transition-all duration-300"></div>
 
--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,6 @@ import aboutme from "./aboutme.png";
 import newaboutpic from "./newaboutpic.png"
 import logo from "./logo.svg";
 import projectspic from "./projectspic.png"
-export { aboutme, logo,newaboutpic,projectspic };
+import project1 from "./project1.png";
+export { aboutme, logo, newaboutpic, projectspic, project1 };
+
